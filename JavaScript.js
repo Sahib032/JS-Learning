5b5b248ed8a22f418f7326c529f860635926bf9c
Sahib <wwwsahib770@gamil.com>
2026-08-09 18:50:00 +0530

    Learning_01

---
+++ JavaScript.js
@@ -0,0 +1,14 @@
+const mob_no = '9999999'
+let otp = Math.floor(100000 + Math.random() * 900000);
+console.log(`Sending OTP ${otp} to mobile number ${mob_no}`);
+
+
+console.log(`OTP sent successfully to ${mob_no}`);
+console.log(otp);
+console.log(`Enter Otp received on ${mob_no}`);
+
+ const stu_id = '20242025'
+ const roll_no = '24MCA1692'
+ let college = 'Jamia Millia Islamia'
+ let Dept = 'Department of Computer Science'
+ console.table([stu_id, roll_no, college, Dept])

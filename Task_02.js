b7c2238a34752cc2a3c8e1f48bbec5dccbad6972
Sahib <wwwsahib770@gamil.com>
2026-08-29 14:43:19 +0530

    Destructuring object

--- Task_02.js
+++ Task_02.js
@@ -30,35 +30,64 @@
 // greet()
 
 //-------Part 17-------
-const user = {
-    name: "Rahul",
-    age: 25,
-    city: "Delhi",
-    isLoggedIn: true
-};
+// const user = {
+//     name: "Rahul",
+//     age: 25,
+//     city: "Delhi",
+//     isLoggedIn: true
+// };
 
-console.log("keys of obj",Object.keys(user))
-console.log("Values of obj",Object.values(user))
-console.log("key-value pair of obj",Object.entries(user))
-console.log("to check email exists or not",user.hasOwnProperty("email"))
-console.log("to check name exists or not",user.hasOwnProperty("name"))
-
-
+// console.log("keys of obj",Object.keys(user))
+// console.log("Values of obj",Object.values(user))
+// console.log("key-value pair of obj",Object.entries(user))
+// console.log("to check email exists or not",user.hasOwnProperty("email"))
+// console.log("to check name exists or not",user.hasOwnProperty("name"))
 
 
+// const personal ={
+//   name: "ABZ",
+//   age:89
+// }
 
+// const professional ={
+//   role:"Developer",
+//   skills:["java","javaScript"]
+// }
 
+// console.log("by using spread() operator :",{...personal,...professional})//spread operator
 
+// console.log("by using Object.assign() :",Object.assign({},personal,professional))
 
+//-------part 18----------
+// Object destructuring - this is a JavaScript syntax introduced in ES6
+// that allows to extract properties from an object and assign them directly to distinct variables in a single,clean line of code
+// const course ={
+//   courseName :"JavaScript",
+//   price:999,
+//   instructor:"ABx"
+// }
+// // Destructuring
 
+// const {courseName,price,instructor}=course
 
+// console.log(courseName);
+// console.log(price);
+// console.log(instructor);
 
-
-
-
-
-
-
+//-------Part 19--------
+// JSON Understanding
+ const response ={
+  success:true,
+  data:{
+    username:"xyz",
+    skills:["Java","Python","React"]
+  }
+ }
+ // {success,data:{username,skills}} , success,data are keys of response,but data itself an object and 
+ // their keys are username and skills, so that's a way to defined destructor of nested object
+const {success,data:{username,skills}}=response
+console.log(username)
+console.log(skills)
 
 
 

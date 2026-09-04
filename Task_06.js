0c73d20872720836539e082d3e2427bb0366ec7c
Sahib <wwwsahib770@gamil.com>
2026-09-04 20:01:22 +0530

    reduce done

---
+++ Task_06.js
@@ -0,0 +1,64 @@
+/* Reduce Method */
+
+// const num = [10, 20, 30, 40, 50];
+// let sum = num.reduce((sum, curr) => {
+//     console.log(`Sum: ${sum}, Current: ${curr}`);
+//     return sum + curr;
+// }, 0);
+// console.log(`Total Sum: ${sum}`);
+
+// let sum = num.reduce((sum, curr) => sum + curr, 0);
+// console.log(`Total Sum: ${sum}`);
+
+/* Map Method */
+
+// const price = [100,200,300,400,500];
+// // const discountedPrice = price.map((p) => p - p * 0.1);
+// // console.log(discountedPrice);
+// const tax = price.map((p) =>p+p*0.18);
+// console.log(tax);
+// const priceWithTax = price.map((p) => ({ original: p, withTax: p * 1.18 }));
+// console.log(priceWithTax);
+
+//calculate product
+// let num = [1, 2, 3, 4, 5];
+// const mul = num.reduce((n1, n2) => n1 * n2, 1);
+// console.log(mul);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
